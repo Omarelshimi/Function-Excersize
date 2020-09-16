479e1d4b2db3bd5229d1fad969d7ba19207947c8
Omar Elshimi <70725794+Omarelshimi@users.noreply.github.com>
2020-09-16 15:29:28 -0600

    Add files via upload

---
+++ function-concept/main.js
@@ -0,0 +1,20 @@
+// Question 2
+function letterGrade(percentGrade) {
+    if (percentGrade >= 80) {
+        return "A"
+    } else if (percentGrade >= 70) {
+        return "B"
+    }  else if (percentGrade >= 60) {
+        return "C"
+    }  else if (percentGrade >= 50) {
+        return "D"
+    }  else if (percentGrade >= 0) {
+        return "F"
+    } 
+}
+// Question 3
+function fahrToCels(Number) {
+    return (Number - 32 * 5 / 9)
+}
+ 
+
